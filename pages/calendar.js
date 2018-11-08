c773b6458868c20aed51eb0ae12b7d451376562d
RScott Blamey <RScottBl@wowlogistics.com>
2018-11-08 16:15:51 -0600

    Add basic appointment calendar table structure

--- pages/calendar.js
+++ pages/calendar.js
@@ -18,16 +18,235 @@ export default class extends Page {
     render() {
         return (
             <Layout title="" pathname={this.props.pathname}>
-
-                <row>
-                    <div class="col-sm-2">
-                        <h2>Calendar Summary</h2>
-                    </div>
-                </row>
-
-                {/* <PageHeader>
-                    WOW Logistics Calendar
-                </PageHeader> */}
+                <table className="table table-striped table-condensed table-hover">
+                    <thead>
+                        <th>Group</th>
+                        <th title="Appointment Time">Time</th>
+                        <th title="Appointment Type">Type</th>
+                        <th title="Appointment Status">Status</th>
+                        <th title="Transportation Carrier Name">Carrier</th>
+                        <th title="Shipper Number">Shipper #</th>
+                        <th title="Confirmation Number">Confirm #</th>
+                        <th title="SCALE Transaction ID">Trans #</th>
+                        <th title="Warehouse Dock Door Number">Door #</th>
+                        <th title="Warehouse Building">Where</th>
+                        <th>Trailer Type</th>
+                        <th>Order Status</th>
+                        <th>Notes</th>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>
+                                <div className="checkbox">
+                                    <label>
+                                        <input type="checkbox" title="Select to group appointments" />
+                                    </label>
+                                </div>
+                            </td>
+                            <td>12:30 A</td>
+                            <td title="Outbound Appointment">Out</td>
+                            <td>Arrived</td>
+                            <td>Schneider Trucking</td>
+                            <td title="Land O Lakes - Wisconsin Rapids Cooler">LOLWRC</td>
+                            <td>SA02200</td>
+                            <td>SAR12345</td>
+                            <td>SHP07</td>
+                            <td>Pittsville</td>
+                            <td>Drop</td>
+                            <td>No Order</td>
+                            <td>Notes go here</td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div className="checkbox">
+                                    <label>
+                                        <input type="checkbox" title="Select to group appointments" />
+                                    </label>
+                                </div>
+                            </td>
+                            <td>12:30 A</td>
+                            <td title="Outbound Appointment">Out</td>
+                            <td>Arrived</td>
+                            <td>Schneider Trucking</td>
+                            <td title="Land O Lakes - Wisconsin Rapids Cooler">LOLWRC</td>
+                            <td>SA02200</td>
+                            <td>SAR12345</td>
+                            <td>SHP07</td>
+                            <td>Pittsville</td>
+                            <td>Drop</td>
+                            <td>No Order</td>
+                            <td>Notes go here</td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div className="checkbox">
+                                    <label>
+                                        <input type="checkbox" title="Select to group appointments" />
+                                    </label>
+                                </div>
+                            </td>
+                            <td>12:30 A</td>
+                            <td title="Outbound Appointment">Out</td>
+                            <td>Arrived</td>
+                            <td>Schneider Trucking</td>
+                            <td title="Land O Lakes - Wisconsin Rapids Cooler">LOLWRC</td>
+                            <td>SA02200</td>
+                            <td>SAR12345</td>
+                            <td>SHP07</td>
+                            <td>Pittsville</td>
+                            <td>Drop</td>
+                            <td>No Order</td>
+                            <td>Notes go here</td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div className="checkbox">
+                                    <label>
+                                        <input type="checkbox" title="Select to group appointments" />
+                                    </label>
+                                </div>
+                            </td>
+                            <td>12:30 A</td>
+                            <td title="Outbound Appointment">Out</td>
+                            <td>Arrived</td>
+                            <td>Schneider Trucking</td>
+                            <td title="Land O Lakes - Wisconsin Rapids Cooler">LOLWRC</td>
+                            <td>SA02200</td>
+                            <td>SAR12345</td>
+                            <td>SHP07</td>
+                            <td>Pittsville</td>
+                            <td>Drop</td>
+                            <td>No Order</td>
+                            <td>Notes go here</td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div className="checkbox">
+                                    <label>
+                                        <input type="checkbox" title="Select to group appointments" />
+                                    </label>
+                                </div>
+                            </td>
+                            <td>12:30 A</td>
+                            <td title="Outbound Appointment">Out</td>
+                            <td>Arrived</td>
+                            <td>Schneider Trucking</td>
+                            <td title="Land O Lakes - Wisconsin Rapids Cooler">LOLWRC</td>
+                            <td>SA02200</td>
+                            <td>SAR12345</td>
+                            <td>SHP07</td>
+                            <td>Pittsville</td>
+                            <td>Drop</td>
+                            <td>No Order</td>
+                            <td>Notes go here</td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div className="checkbox">
+                                    <label>
+                                        <input type="checkbox" title="Select to group appointments" />
+                                    </label>
+                                </div>
+                            </td>
+                            <td>12:30 A</td>
+                            <td title="Outbound Appointment">Out</td>
+                            <td>Arrived</td>
+                            <td>Schneider Trucking</td>
+                            <td title="Land O Lakes - Wisconsin Rapids Cooler">LOLWRC</td>
+                            <td>SA02200</td>
+                            <td>SAR12345</td>
+                            <td>SHP07</td>
+                            <td>Pittsville</td>
+                            <td>Drop</td>
+                            <td>No Order</td>
+                            <td>Notes go here</td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div className="checkbox">
+                                    <label>
+                                        <input type="checkbox" title="Select to group appointments" />
+                                    </label>
+                                </div>
+                            </td>
+                            <td>12:30 A</td>
+                            <td title="Outbound Appointment">Out</td>
+                            <td>Arrived</td>
+                            <td>Schneider Trucking</td>
+                            <td title="Land O Lakes - Wisconsin Rapids Cooler">LOLWRC</td>
+                            <td>SA02200</td>
+                            <td>SAR12345</td>
+                            <td>SHP07</td>
+                            <td>Pittsville</td>
+                            <td>Drop</td>
+                            <td>No Order</td>
+                            <td>Notes go here</td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div className="checkbox">
+                                    <label>
+                                        <input type="checkbox" title="Select to group appointments" />
+                                    </label>
+                                </div>
+                            </td>
+                            <td>12:30 A</td>
+                            <td title="Outbound Appointment">Out</td>
+                            <td>Arrived</td>
+                            <td>Schneider Trucking</td>
+                            <td title="Land O Lakes - Wisconsin Rapids Cooler">LOLWRC</td>
+                            <td>SA02200</td>
+                            <td>SAR12345</td>
+                            <td>SHP07</td>
+                            <td>Pittsville</td>
+                            <td>Drop</td>
+                            <td>No Order</td>
+                            <td>Notes go here</td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div className="checkbox">
+                                    <label>
+                                        <input type="checkbox" title="Select to group appointments" />
+                                    </label>
+                                </div>
+                            </td>
+                            <td>12:30 A</td>
+                            <td title="Outbound Appointment">Out</td>
+                            <td>Arrived</td>
+                            <td>Schneider Trucking</td>
+                            <td title="Land O Lakes - Wisconsin Rapids Cooler">LOLWRC</td>
+                            <td>SA02200</td>
+                            <td>SAR12345</td>
+                            <td>SHP07</td>
+                            <td>Pittsville</td>
+                            <td>Drop</td>
+                            <td>No Order</td>
+                            <td>Notes go here</td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div className="checkbox">
+                                    <label>
+                                        <input type="checkbox" title="Select to group appointments" />
+                                    </label>
+                                </div>
+                            </td>
+                            <td>12:30 A</td>
+                            <td title="Outbound Appointment">Out</td>
+                            <td>Arrived</td>
+                            <td>Schneider Trucking</td>
+                            <td title="Land O Lakes - Wisconsin Rapids Cooler">LOLWRC</td>
+                            <td>SA02200</td>
+                            <td>SAR12345</td>
+                            <td>SHP07</td>
+                            <td>Pittsville</td>
+                            <td>Drop</td>
+                            <td>No Order</td>
+                            <td>Notes go here</td>
+                        </tr>
+                    </tbody>
+                </table>
             </Layout>
         )
     }
