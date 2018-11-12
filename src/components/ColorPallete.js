494dde2f5c6008ba2f510499365bf9786f0eaf0b
RScott Blamey <RScottBl@wowlogistics.com>
2018-11-12 16:08:31 -0600

    Remove unused colors

--- src/components/ColorPallete.js
+++ src/components/ColorPallete.js
@@ -5,8 +5,8 @@ class ColorPallete extends React.Component {
     render() {
         return (               
                 <div className="ColorPallete clearfix">
-                    <p className="small">Note: 5% darker shade displayed in lower right corner of swatches represents hover color.</p>
                     <h2>Primary Brand Colors</h2>
+                    <p className="small">Note: 5% darker shade displayed in lower right corner of swatches represents hover color.</p>
                     <ul className="theme-colors clearfix">
                         <li className="brand-primary">
                             <div className="example">
@@ -53,82 +53,7 @@ class ColorPallete extends React.Component {
                             </div>
                         </li>
                     </ul>
-                    
-                    <h2>Secondary Brand Colors</h2>
-                    <ul className="theme-colors clearfix">
-                        <li className="brand2-yellow">
-                            <div className="example">
-                                <span className="color-swatch"></span>
-                            </div>
-                            <div className="info">
-                                <span className="color-title" title="Color Name">Brand Secondary Yellow</span>
-                                <span className="color-hex" title="HEX Code">#fdda24</span>
-                                <span className="color-rgb" title="Red Green Blue"><span className="rgb-red">253</span><span className="rgb-green">218</span><span className="rgb-blue">36</span></span>
-                                <span className="color-var" title="LESS Variable">@brand2-yellow</span>
-                            </div>
-                        </li>
-                        <li className="brand2-green">
-                            <div className="example">
-                                <span className="color-swatch"></span>
-                            </div>
-                            <div className="info">
-                                <span className="color-title" title="Color Name">Brand Secondary Green</span>
-                                <span className="color-hex" title="HEX Code">#74aa50</span>
-                                <span className="color-rgb" title="Red Green Blue"><span className="rgb-red">116</span><span className="rgb-green">170</span><span className="rgb-blue">80</span></span>
-                                <span className="color-var" title="LESS Variable">@brand2-green</span>
-                            </div>
-                        </li>
-                        <li className="brand2-blue">
-                            <div className="example">
-                                <span className="color-swatch"></span>
-                            </div>
-                            <div className="info">
-                                <span className="color-title" title="Color Name">Brand Secondary Blue / Information</span>
-                                <span className="color-hex" title="HEX Code">#63acb4</span>
-                                <span className="color-rgb" title="Red Green Blue"><span className="rgb-red">99</span><span className="rgb-green">172</span><span className="rgb-blue">180</span></span>
-                                <span className="color-var" title="LESS Variable">@brand-info</span>
-                                <span className="color-var" title="LESS Variable">@brand2-blue</span>
-                            </div>
-                        </li>
-                    </ul>
-                    
-                    <h2>Medicare Brand Colors</h2>
-                    <ul className="theme-colors clearfix">
-                        <li className="medicare-red">
-                            <div className="example">
-                                <span className="color-swatch"></span>
-                            </div>
-                            <div className="info">
-                                <span className="color-title" title="Color Name">Medicare Red</span>
-                                <span className="color-hex" title="HEX Code">#ad2624</span>
-                                <span className="color-rgb" title="Red Green Blue"><span className="rgb-red">173</span><span className="rgb-green">38</span><span className="rgb-blue">36</span></span>
-                                <span className="color-var" title="LESS Variable">@medicare-red</span>
-                            </div>
-                        </li>
-                        <li className="medicare-purple">
-                            <div className="example">
-                                <span className="color-swatch"></span>
-                            </div>
-                            <div className="info">
-                                <span className="color-title" title="Color Name">Medicare Purple</span>
-                                <span className="color-hex" title="HEX Code">#6d2077</span>
-                                <span className="color-rgb" title="Red Green Blue"><span className="rgb-red">109</span><span className="rgb-green">32</span><span className="rgb-blue">119</span></span>
-                                <span className="color-var" title="LESS Variable">@medicare-purple</span>
-                            </div>
-                        </li>
-                        <li className="medicare-teal">
-                            <div className="example">
-                                <span className="color-swatch"></span>
-                            </div>
-                            <div className="info">
-                                <span className="color-title" title="Color Name">Medicare Teal</span>
-                                <span className="color-hex" title="HEX Code">#458a94</span>
-                                <span className="color-rgb" title="Red Green Blue"><span className="rgb-red">69</span><span className="rgb-green">138</span><span className="rgb-blue">148</span></span>
-                                <span className="color-var" title="LESS Variable">@medicare-teal</span>
-                            </div>
-                        </li>
-                    </ul>
-                    
+    
                     <h2>Grays</h2>
                     <ul className="theme-colors clearfix">
                         <li className="gray-base">
