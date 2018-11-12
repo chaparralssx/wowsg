81dd8c591a1f8dd7a5d1ea42c6e4eae081f76f42
RScott Blamey <RScottBl@wowlogistics.com>
2018-11-12 16:02:53 -0600

    Remove unused elements

--- src/components/Masthead.js
+++ src/components/Masthead.js
@@ -1,7 +1,6 @@
 import {Row} from 'react-bootstrap/lib'
 import React from 'react'
 import link from '../utils/bootstrapLink'
-import PrimaryNav from './PrimaryNav'
 
 class Masthead extends React.Component {
     render() {
@@ -10,14 +9,6 @@ class Masthead extends React.Component {
                     <div className="brand">
                         <a onClick={(e) => link.handleLink(e, '/')} href="/"><img src="/static/images/logo-wow-logistics.png" alt="WOW Logistics Logo" title="Return to the homepage" /></a>
                     </div>
-                    {/* <div className="information">
-                        Questions? Call <strong>888-888-8888</strong> to reach a sales representative
-                    </div>
-                    <div className="information">
-                        <div className="title" id="application-title">Application Title or Marketing Message</div>
-                        <div className="small">Optional description or teaser text</div>
-                    </div> */}
-                    <PrimaryNav/>
                 </div>
         )
     }
