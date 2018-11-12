a3cea152eb8b5c8ecaec214fc689c2b1fbd421b4
RScott Blamey <RScottBl@wowlogistics.com>
2018-11-12 16:06:38 -0600

    Remove unused elements

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,11 +1,7 @@
 import Head from 'next/head'
-import Masthead from './Masthead'
 // import PrimaryNav from './PrimaryNav'
-import Footer from './Footer'
 import TrackDuck from './TrackDuck'
 import {Grid, Row} from 'react-bootstrap/lib'
-import GlobalNavbar from './GlobalNavbar'
-// import Carousel from './Carousel'
 import React from 'react'
 import Router from 'next/router'
 import NProgress from 'nprogress'
@@ -38,10 +34,6 @@ export default ({ children, title = '', pathname, className }) => (
         
 
         <Grid>
-
-            <Row>            
-                <Masthead/>
-            </Row>
 
             <Row className="maincontent" role="main">
                 { children }
