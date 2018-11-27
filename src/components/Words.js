934a1a0b2464313d41297e046de6f54068c1a8c0
RScott Blamey <RScottBl@wowlogistics.com>
2018-11-27 10:03:20 -0600

    Add terms that should always be UPPERCASE or Capitalized

--- src/components/Words.js
+++ src/components/Words.js
@@ -3,12 +3,39 @@ import React from 'react';
 class Words extends React.Component {
     render() {
         return (
-            <div className="Words clearfix">
+            <section className="Words clearfix">
                 <h2>Words</h2>
-                <h3>Abbreviations</h3>
-                <dt>Number</dt>
-                <dd>Use "#" not "No.", "No", "no"</dd>
-            </div>
+
+                <article>
+                    <h3>WOW Terms</h3>
+
+                    <p>Always use UPPERCASE for</p>
+                    <ul>
+                        <li>SCALE</li>
+                        <li>LPN</li>
+                        <li>RF</li>
+                    </ul>
+
+                    <p>Always Capitalize these WMS (Calendar, RF, Portal) words no matter where they are used in sentences</p>
+                    <ul>
+                        <li>Calendar</li>
+                        <li>Portal</li>
+                        <li>Line</li>
+                        <li>Lot</li>
+                    </ul>
+                </article>
+
+                <article className="unapproved">
+                    <h3>Industry Terms</h3>
+                </article>
+
+                <article>
+                    <h3>Abbreviations</h3>
+                    <dt>Number</dt>
+                    <dd>Use "#" not "No.", "No", "no"</dd>
+                </article>
+
+            </section>
         )
     }
 }
