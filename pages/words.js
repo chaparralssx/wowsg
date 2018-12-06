db6f9bdfeb4420542963575036a07c3b69e795e2
RScott Blamey <RScottBl@wowlogistics.com>
2018-12-06 10:11:45 -0600

    Add Hyphanation and Spacing sections

--- pages/words.js
+++ pages/words.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Layout from '../src/components/Layout'
 import Page from '../src/components/Page'
-import Words from '../src/components/Words'
 
 
 export default class extends Page {
@@ -12,7 +11,7 @@ export default class extends Page {
                     <h2>Words</h2>
 
                     <article>
-                        <h3>WOW Terms</h3>
+                        <h3>Capitalization</h3>
 
                         <p>Always use UPPERCASE for</p>
                         <ul>
@@ -21,12 +20,36 @@ export default class extends Page {
                             <li>RF</li>
                         </ul>
 
-                        <p>Always Capitalize these WMS (Calendar, RF, Portal) words no matter where they are used in sentences</p>
+                        <p>Always Capitalize these words when they refer to the WOW or SCALE systems</p>
                         <ul>
                             <li>Calendar</li>
                             <li>Portal</li>
                             <li>Line</li>
                             <li>Lot</li>
+                        </ul>
+                    </article>
+
+                    <article>
+                        <h3>Hyphenation</h3>
+                        <p>Always hyphenate these words</p>
+                        <ul>
+                            <li>Re-print (not Reprint)</li>
+                        </ul>
+                        <p>Do not hyphenate these words</p>
+                        <ul>
+                            <li>Checklist</li>
+                        </ul>
+                    </article>
+
+                    <article>
+                        <h3>Spaces in Words</h3>
+                        <p>Always place a space in these words</p>
+                        <ul>
+                            <li></li>
+                        </ul>
+                        <p>Do not place a space in these words</p>
+                        <ul>
+                            <li>Checklist</li>
                         </ul>
                     </article>
 
