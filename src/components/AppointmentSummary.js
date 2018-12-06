ae4ff0f4eb2fae88004bef14f86c4cf479625b04
RScott Blamey <RScottBl@wowlogistics.com>
2018-12-06 07:45:00 -0600

    Add AppointmentSummary component

---
+++ src/components/AppointmentSummary.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class AppointmentSummary extends React.Component {
+    render() {
+        return (
+            <div className="AppointmentSummary">
+                Appointments Summary
+            </div>
+        )
+    }
+}
+
+export default AppointmentSummary;
