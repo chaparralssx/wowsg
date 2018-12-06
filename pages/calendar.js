cc516b4faaed1f2a47650df03e77257b82be4d3d
RScott Blamey <RScottBl@wowlogistics.com>
2018-12-06 07:46:14 -0600

    Add Dahsboard left col skeleton components

--- pages/calendar.js
+++ pages/calendar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Layout from '../src/components/CalendarLayout'
 import Calendar from '../src/components/Calendar'
+import AppointmentSummary from '../src/components/AppointmentSummary'
 import Page from '../src/components/Page'
 
 export default class extends Page {
@@ -17,8 +18,31 @@ export default class extends Page {
 
     render() {
         return (
-            <Layout title="" pathname={this.props.pathname}>
-                <Calendar></Calendar>
+            <Layout title="Dashboard" pathname={this.props.pathname}>
+                <h1 className="hidden">Dashboard</h1>
+                <div className="col-sm-2">
+                    <div className="dashboardApps">
+                        <h2 className="hidden">Dashboard Applications</h2>
+                        <ul className="nav nav-tabs" role="tablist">
+                            <li role="presentation" className="active"><a href="#summary" aria-controls="summary" role="tab" data-toggle="tab">Summary</a></li>
+                            <li role="presentation"><a href="#group" aria-controls="group" role="tab" data-toggle="tab">Group</a></li>
+                            <li role="presentation"><a href="#search" aria-controls="search" role="tab" data-toggle="tab">Search</a></li>
+                            <li role="presentation"><a href="#doors" aria-controls="doors" role="tab" data-toggle="tab">Doors</a></li>
+                            <li role="presentation"><a href="#log" aria-controls="log" role="tab" data-toggle="tab">Log</a></li>
+                        </ul>
+
+                        <div className="tab-content">
+                            <div role="tabpanel" className="tab-pane active" id="summary"><AppointmentSummary /></div>
+                            <div role="tabpanel" className="tab-pane" id="group">Group/UnGroup</div>
+                            <div role="tabpanel" className="tab-pane" id="search">Search</div>
+                            <div role="tabpanel" className="tab-pane" id="doors">Door Queue</div>
+                            <div role="tabpanel" className="tab-pane" id="log">Message Log</div>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-sm-10">                    
+                    <Calendar></Calendar>
+                </div>
             </Layout>
         )
     }
