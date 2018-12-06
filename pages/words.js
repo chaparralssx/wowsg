62176b5845da808c78a18192407896420fe55fbe
RScott Blamey <RScottBl@wowlogistics.com>
2018-12-06 10:06:09 -0600

    Move HTML from Words component to Words page

--- pages/words.js
+++ pages/words.js
@@ -8,7 +8,39 @@ export default class extends Page {
     render() {
         return (
             <Layout title="Words | " pathname={this.props.pathname}>
-                <Words/>
+                <section className="Words clearfix">
+                    <h2>Words</h2>
+
+                    <article>
+                        <h3>WOW Terms</h3>
+
+                        <p>Always use UPPERCASE for</p>
+                        <ul>
+                            <li>SCALE</li>
+                            <li>LPN</li>
+                            <li>RF</li>
+                        </ul>
+
+                        <p>Always Capitalize these WMS (Calendar, RF, Portal) words no matter where they are used in sentences</p>
+                        <ul>
+                            <li>Calendar</li>
+                            <li>Portal</li>
+                            <li>Line</li>
+                            <li>Lot</li>
+                        </ul>
+                    </article>
+
+                    <article className="unapproved">
+                        <h3>Industry Terms</h3>
+                    </article>
+
+                    <article>
+                        <h3>Abbreviations</h3>
+                        <dt>Number</dt>
+                        <dd>Use "#" not "No.", "No", "no"</dd>
+                    </article>
+
+                </section>
             </Layout>
         )
     }
